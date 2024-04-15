02038c1f8f8badaabd7cd3d50982deb5148206ef
Joselu <joseluissgz97@gmail.com>
2024-04-15 20:42:36 +0200

    add: Barril to export Pages

---
+++ src/pages/index.ts
@@ -0,0 +1,4 @@
+export { default as Bookings } from './Bookings';
+export { default as Contact } from './Contact';
+export { default as Dashboard } from './Dashboard';
+export { default as Rooms } from './Rooms';
