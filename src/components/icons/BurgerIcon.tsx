d7adbc0ca2d7af71634f4d316660d2effadd50c5
Joselu <joseluissgz97@gmail.com>
2024-04-15 20:39:40 +0200

    add: Icons

---
+++ src/components/icons/BurgerIcon.tsx
@@ -0,0 +1,17 @@
+const BurgerIcon = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg {...props} viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+    <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+    <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
+    <g id="SVGRepo_iconCarrier">
+      <path
+        d="M4 6H20M4 12H14M4 18H9"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      ></path>
+    </g>
+  </svg>
+);
+
+export default BurgerIcon;
