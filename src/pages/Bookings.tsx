afa798bcf3dc4223bc7281b7ea1b39a6ba4b2cae
Joselu <joseluissgz97@gmail.com>
2024-04-16 22:42:03 +0200

    add: containerSection

--- src/pages/Bookings.tsx
+++ src/pages/Bookings.tsx
@@ -1,5 +1,17 @@
+import styled from 'styled-components';
+import Header from '../components/Header';
+
 const Bookings = () => {
-  return <h2>Bookings</h2>;
+  return (
+    <ContainerSection>
+      <Header />
+      <h2>Bookings</h2>
+    </ContainerSection>
+  );
 };
 
 export default Bookings;
+
+const ContainerSection = styled.section`
+  background-color: #171717;
+`;
