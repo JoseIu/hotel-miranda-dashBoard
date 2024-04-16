5d486900b6542ffa3d3b2057edd882f761ac6790
Joselu <joseluissgz97@gmail.com>
2024-04-16 22:42:10 +0200

    add: ContainerSection

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,5 +1,17 @@
+import styled from 'styled-components';
+import Header from '../components/Header';
+
 const Contact = () => {
-  return <h2>Contact</h2>;
+  return (
+    <ContainerSection>
+      <Header />
+      <h2>Contact</h2>
+    </ContainerSection>
+  );
 };
 
 export default Contact;
+
+const ContainerSection = styled.section`
+  background-color: #171717;
+`;
