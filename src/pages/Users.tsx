5bed90da6889c82d9505f2f70c2f1b2817b25a59
Joselu <joseluissgz97@gmail.com>
2024-04-16 22:43:33 +0200

    add: Link to User Page

--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -1,5 +1,19 @@
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import Header from '../components/Header';
+
 const Users = () => {
-  return <h2>Users</h2>;
+  return (
+    <ContainerSection>
+      <Header />
+      <h2>Users</h2>
+
+      <Link to={`/users/${1}`}>EACH USER</Link>
+    </ContainerSection>
+  );
 };
 
 export default Users;
+const ContainerSection = styled.section`
+  background-color: #171717;
+`;
