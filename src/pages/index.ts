34a587c4249cd812d7aed32a08d38e1abcba62b2
Joselu <joseluissgz97@gmail.com>
2024-04-18 13:01:07 +0200

    fix: export

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,6 +1,6 @@
 export { default as Bookings } from './Bookings';
 export { default as Contact } from './Contact';
-export { default as Dashboard } from './Dashboard';
+export { default as DashboardPage } from './DashboardPage';
 export { default as Room } from './Room';
 export { default as Rooms } from './Rooms';
 export { default as User } from './User';
