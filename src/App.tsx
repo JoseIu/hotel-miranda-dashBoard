6c858d194d15d7d49565ca6dcfcd52345c3897a9
Joselu <joseluissgz97@gmail.com>
2024-04-18 12:57:22 +0200

    reafctor: refacotring Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,36 @@
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Navigation from './routes/Navigation';
+import { AuthProvider } from './context/authProvides';
+import { Bookings, Contact, DashboardPage, Room, Rooms, User } from './pages';
+import Login from './pages/Login';
+import Users from './pages/Users';
+import DashBoard from './routes/DashBoard';
+import { routes } from './routes/routes';
+import AuthTemplate from './template/AuthTemplate';
 
 const App = () => {
   return (
-    <>
-      <Navigation />
-    </>
+    <BrowserRouter>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<AuthTemplate />}>
+            <Route index element={<Login />} />
+          </Route>
+
+          <Route path="/admin/*" element={<DashBoard />}>
+            <Route index path="dashboard" element={<DashboardPage />} />
+            <Route path="bookings" element={<Bookings />} />
+            <Route path="rooms" element={<Rooms />} />
+            <Route path="contact" element={<Contact />} />
+            <Route path="users" element={<Users />} />
+
+            <Route path="rooms/:id" element={<Room />} />
+            <Route path="users/:id" element={<User />} />
+            <Route path="*" element={<Navigate to={routes[0].to} replace />} />
+          </Route>
+        </Routes>
+      </AuthProvider>
+    </BrowserRouter>
   );
 };
 
