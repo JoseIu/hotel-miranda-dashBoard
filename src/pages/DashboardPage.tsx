be8a68d6347ae793c33c93187a7b1bafb8511873
Joselu <joseluissgz97@gmail.com>
2024-04-18 12:59:21 +0200

    change: chaging Dahsboard to DashboardPage

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,17 +1,17 @@
 import styled from 'styled-components';
 import Header from '../components/Header';
 
-const Dashboard = () => {
+const DashboardPage = () => {
   return (
-    <DashboardPage>
+    <Container>
       <Header />
       <h2>Dashboard PAGE</h2>
-    </DashboardPage>
+    </Container>
   );
 };
 
-export default Dashboard;
+export default DashboardPage;
 
-const DashboardPage = styled.section`
+const Container = styled.section`
   background-color: #171717;
 `;
