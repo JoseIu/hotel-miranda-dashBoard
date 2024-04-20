2cd13a5315acd080af390e22dc834bf4f0443cd6
Joselu <joseluissgz97@gmail.com>
2024-04-20 21:48:57 +0200

    add: BedIcon

---
+++ src/components/icons/BedIcon.tsx
@@ -0,0 +1,11 @@
+const BedIcon = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg {...props} fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+    <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+    <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
+    <g id="SVGRepo_iconCarrier">
+      <path d="M21,12.184V4a1,1,0,0,0-2,0V5H5V4A1,1,0,0,0,3,4v8.184A3,3,0,0,0,1,15v5a1,1,0,0,0,2,0V19H21v1a1,1,0,0,0,2,0V15A3,3,0,0,0,21,12.184ZM19,12H18V10a1,1,0,0,0-1-1H7a1,1,0,0,0-1,1v2H5V7H19ZM8,12V11h3v1Zm5-1h3v1H13ZM3,15a1,1,0,0,1,1-1H20a1,1,0,0,1,1,1v2H3Z"></path>
+    </g>
+  </svg>
+);
+
+export default BedIcon;
