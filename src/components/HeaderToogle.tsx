897c2d5dd1b835e809e8af01c6a7b9716b34bf45
Joselu <joseluissgz97@gmail.com>
2024-04-22 21:42:48 +0200

    add: HeaderToogle to show buttons to close or opne aside menu

---
+++ src/components/HeaderToogle.tsx
@@ -0,0 +1,40 @@
+import styled from 'styled-components';
+import useMenu from '../hooks/useMenu';
+import ArrowLeft from './icons/ArrowLeft';
+import ArrowRight from './icons/ArrowRight';
+
+const HeaderToogle = () => {
+  const { isActived, setIsActived } = useMenu();
+
+  return (
+    <>
+      {isActived ? (
+        <ButtonToogle aria-label="button to show menu" onClick={() => setIsActived(!isActived)}>
+          <ArrowRight />
+        </ButtonToogle>
+      ) : (
+        <ButtonToogle aria-label="button to hidden menu" onClick={() => setIsActived(!isActived)}>
+          <ArrowLeft />
+        </ButtonToogle>
+      )}
+    </>
+  );
+};
+
+export default HeaderToogle;
+
+const ButtonToogle = styled.button`
+
+
+    transition: all 0.3s ease;
+    cursor: pointer;
+    &:hover {
+      transform: scale(1.2);
+    }
+  }
+  svg {
+    width: 1.75rem;
+    color: #ffffff;
+  
+
+`;
