21dae643bc23f097ab1d566d17e47916c9ba82bf
Joselu <joseluissgz97@gmail.com>
2024-04-26 21:45:13 +0200

    add: new asyncRequestFake

--- src/helpers/asyncRequest.ts
+++ src/helpers/asyncRequest.ts
@@ -2,10 +2,19 @@ interface AsyncRequest<T> {
   data: T[];
   delay?: number;
 }
+
 export const asyncRequest = <T>({ data, delay = 200 }: AsyncRequest<T>) => {
   return new Promise((resolve) => {
     setTimeout(() => {
       resolve(data);
     }, delay);
+  });
+};
+
+export const asyncRequestFake = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(true);
+    }, 200);
   });
 };
