7f9e1209f3feaa121486e0b9133f180117548146
Joselu <joseluissgz97@gmail.com>
2024-04-26 21:44:09 +0200

    add:roomsSlice with extraReducers

---
+++ src/features/roomsSlice/roomsSlice.ts
@@ -0,0 +1,42 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { Room } from '../../interfaces/room';
+import { getAllRooms, getRom } from './roomsThunk';
+
+interface Bookings {
+  rooms: Room[];
+  room: Room | null;
+  loading: 'idle' | 'pending' | 'succeeded' | 'failed';
+}
+const initialState: Bookings = {
+  rooms: [],
+  room: null,
+  loading: 'idle',
+};
+
+export const roomsSlice = createSlice({
+  name: 'rooms',
+  initialState,
+  reducers: {},
+
+  extraReducers: (builder) => {
+    builder.addCase(getAllRooms.pending, (state) => {
+      state.loading = 'pending';
+    });
+    builder.addCase(getAllRooms.fulfilled, (state, action) => {
+      state.rooms = action.payload;
+      state.loading = 'succeeded';
+    });
+    builder.addCase(getAllRooms.rejected, (state) => {
+      state.loading = 'failed';
+    });
+
+    builder.addCase(getRom.fulfilled, (state, action) => {
+      const findRoom = state.rooms.find((room) => room.room.id === action.payload);
+      if (!findRoom) return;
+      state.room = findRoom;
+      state.loading = 'succeeded';
+    });
+  },
+});
+
+// export const { increment } = roomsSlice.actions;
