02fa0d91ee87b19370675e37701dbcab396b2583
Joselu <joseluissgz97@gmail.com>
2024-04-27 23:23:30 +0200

    change: moving getRooms inseide useEffect and adding dispatch as dependencie

--- src/pages/RoomsPage.tsx
+++ src/pages/RoomsPage.tsx
@@ -22,19 +22,19 @@ const RoomsPage = () => {
   const [loading, setLoading] = useState(true);
   const { rooms } = useSelector((state: RootState) => state.rooms);
   const dispatch = useDispatch<AppDispatch>();
-  const getRooms = async () => {
-    await dispatch(getAllRooms()).unwrap();
-    setLoading(false);
-  };
 
   const { setTypeSort, filters } = useFilters();
-  console.log(rooms);
   const roomsFiltered = roomsSorted(rooms, filters.typeSort);
-  useEffect(() => {
-    getRooms();
-  }, []);
 
-  if (loading || !rooms.length) {
+  useEffect(() => {
+    const getRooms = async () => {
+      await dispatch(getAllRooms()).unwrap();
+      setLoading(false);
+    };
+    getRooms();
+  }, [dispatch]);
+
+  if (loading) {
     return <div>Loading...</div>;
   }
   return (
@@ -42,37 +42,36 @@ const RoomsPage = () => {
       <Header title={'Rooms'} />
       <Wrapper>
         <RoomsSort>
-          <SortButtonActive $active={filters.sortActive === 1} onClick={() => setTypeSort(1)}>
+          <SortButtonActive $active={filters.sortActive === 0} onClick={() => setTypeSort(1)}>
             All Rooms
           </SortButtonActive>
-          <SortButtonActive $active={filters.sortActive === 2} onClick={() => setTypeSort(2)}>
+          <SortButtonActive $active={filters.sortActive === 1} onClick={() => setTypeSort(2)}>
             Status
           </SortButtonActive>
-          <SortButtonActive $active={filters.sortActive === 3} onClick={() => setTypeSort(3)}>
+          <SortButtonActive $active={filters.sortActive === 2} onClick={() => setTypeSort(3)}>
             Price
           </SortButtonActive>
         </RoomsSort>
         <Table columns={columns}>
-          {roomsFiltered &&
-            roomsFiltered.map((room) => (
-              <Row key={room.room.id}>
-                <Link to={`/admin/rooms/${room.room.id}`}>
-                  <TableGuest
-                    img={room.room.image}
-                    id={room.room.id}
-                    name={room.roomType}
-                    lastName={room.room.number}
-                  />
-                </Link>
-                <td>{room.roomType}</td>
-                <td> {room.amenities} </td>
+          {roomsFiltered.map((room) => (
+            <Row key={room.room.id}>
+              <Link to={`/admin/rooms/${room.room.id}`}>
+                <TableGuest
+                  img={room.room.image}
+                  id={room.room.id}
+                  name={room.roomType}
+                  lastName={room.room.number}
+                />
+              </Link>
+              <td>{room.roomType}</td>
+              <td> {room.amenities} </td>
 
-                <td>{room.price} </td>
+              <td>{room.price} </td>
 
-                <td>{room.offer} </td>
-                <RoomStatus $status={room.status}>{room.status ? 'Disponible' : 'Ocupada'} </RoomStatus>
-              </Row>
-            ))}
+              <td>{room.offer} </td>
+              <RoomStatus $status={room.status}>{room.status ? 'Disponible' : 'Ocupada'} </RoomStatus>
+            </Row>
+          ))}
         </Table>
       </Wrapper>
     </ContainerSection>
@@ -106,19 +105,18 @@ const useFilters = () => {
 };
 
 const roomsSorted = (rooms: Room[], typeSort: number) => {
-  if (!rooms.length) return [...rooms];
   const roomsToSort = [...rooms];
 
   switch (typeSort) {
+    case 0:
+      return roomsToSort.sort((a, b) => Number(a.room?.number) - Number(b.room?.number));
     case 1:
-      return roomsToSort.sort((a, b) => Number(a.room.number) - Number(b.room.number));
-    case 2:
       return roomsToSort.sort((a, b) => {
         if (a.status === b.status) return 0;
         if (a.status && !b.status) return -1;
         return 1;
       });
-    case 3:
+    case 2:
       return roomsToSort.sort((a, b) => a.price - b.price);
     default:
       return roomsToSort;
