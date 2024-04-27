e590c69ce26e7c7eaf212e7a00ce8855d187c87d
Joselu <joseluissgz97@gmail.com>
2024-04-27 23:22:58 +0200

    add: loginPage styles

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -24,35 +24,39 @@ const LoginPage = () => {
   }, [userData.isAuthenticated, navigate]);
 
   return (
-    <LoginContainer className="wrapper">
-      <h2>WellCome to Hotel Miranda Dashboard</h2>
+    <LoginContainer>
+      <div className="wrapper">
+        <h2>WellCome to Hotel Miranda Dashboard</h2>
 
-      <form onSubmit={handleSumit}>
-        <input
-          type="email"
-          placeholder="email"
-          name="email"
-          id="email"
-          autoComplete="email"
-          onChange={(event) => setEmail(event.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="password"
-          name="password"
-          id="password"
-          autoComplete="current-password"
-          onChange={(event) => setPassword(event.target.value)}
-        />
-        <input type="submit" value="Login" />
-      </form>
+        <LoginForm onSubmit={handleSumit}>
+          <input
+            type="email"
+            placeholder="email"
+            name="email"
+            id="email"
+            autoComplete="email"
+            value={form.email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="password"
+            name="password"
+            id="password"
+            autoComplete="current-password"
+            value={form.password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+          <button type="submit">Login</button>
+        </LoginForm>
+      </div>
     </LoginContainer>
   );
 };
 
 export default LoginPage;
 const useForm = () => {
-  const [form, setForm] = useState({ email: '', password: '' });
+  const [form, setForm] = useState({ email: 'prueba@prueba.com', password: 'prueba' });
 
   const setEmail = (email: string) => {
     setForm({ ...form, email });
@@ -65,11 +69,49 @@ const useForm = () => {
 };
 
 const LoginContainer = styled.section`
-  background-color: #171717;
+  background-color: #18181b;
   height: 100dvh;
+
+  display: flex;
+  justify-content: center;
   h2 {
     text-transform: uppercase;
     font-weight: 600;
-    font-size: 3em;
+    font-size: clamp(2rem, 3vw, 3.5rem);
+    text-align: center;
+    margin-bottom: 10rem;
+  }
+`;
+
+const LoginForm = styled.form`
+  max-width: 50rem;
+  padding: 2rem;
+  margin-inline: auto;
+
+  background-color: linear-gradient(145deg, #161618, #1a1a1d);
+  box-shadow: 5px 5px 10px #141417, -5px -5px 10px #1c1c1f;
+  border-radius: 0.5rem;
+
+  input {
+    width: 100%;
+    padding: 0.5rem 1rem;
+    margin-bottom: 1.5rem;
+    border-radius: 0.3rem;
+    outline: none;
+    box-shadow: 2px 2px 5px #141417, -0.5px -0.5px 3px #1c1c1f;
+    &:focus {
+      box-shadow: 3px 3px 10px #1c1c1f, -1px -1px 6px #141417;
+    }
+  }
+  button {
+    box-shadow: 5px 5px 10px #141417, -5px -5px 10px #1c1c1f;
+    padding: 0.5rem 2rem;
+    border-radius: 0.3rem;
+    margin-inline: auto;
+    cursor: pointer;
+    transition: all 0.3s ease-in-out;
+    &:hover {
+      transform: scale(1.1);
+    }
   }
 `;
