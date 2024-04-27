0b3ed795145e6da01a39888da0f24b6a77116983
Joselu <joseluissgz97@gmail.com>
2024-04-27 23:21:35 +0200

    add: UsersSlice and thunks

--- src/features/usersSlice/usersSlice.ts
+++ src/features/usersSlice/usersSlice.ts
@@ -1,9 +1,33 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { Employee } from '../../interfaces/employee.interface';
+import { getUsers } from './usersThunk';
+
+interface Bookings {
+  users: Employee[];
+  user: Employee | null;
+  loading: 'idle' | 'pending' | 'succeeded' | 'failed';
+}
+const initialState: Bookings = {
+  users: [],
+  user: null,
+  loading: 'idle',
+};
 
 export const usersSlice = createSlice({
   name: 'users',
-  initialState: {
-    counter: 10,
-  },
+  initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(getUsers.pending, (state) => {
+      state.loading = 'pending';
+    });
+    builder.addCase(getUsers.fulfilled, (state, action) => {
+      state.users = action.payload;
+      state.loading = 'succeeded';
+    });
+
+    builder.addCase(getUsers.rejected, (state) => {
+      state.loading = 'failed';
+    });
+  },
 });
