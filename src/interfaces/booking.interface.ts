df81ec33d553a42244157f07363fda375a9f5763
Joselu <joseluissgz97@gmail.com>
2024-05-28 23:11:22 +0200

    feat: Add BookingInterface for managing bookings

---
+++ src/interfaces/booking.interface.ts
@@ -0,0 +1,25 @@
+export interface BookingInterface {
+  _id: string;
+  guest: Guest;
+  checkin: Check;
+  checkOut: Check;
+  orderDate: string;
+  roomType: string;
+  roomNumber: string;
+  roomID: string;
+  specialRequest: string;
+  status: Status;
+}
+
+export interface Check {
+  date: string;
+  time: string;
+}
+
+export interface Guest {
+  name: string;
+  lastName: string;
+  reservationID: string;
+  img: string;
+}
+export type Status = 'Check In' | 'Check Out' | 'In Progress';
