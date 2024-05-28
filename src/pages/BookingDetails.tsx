2ea1d8e74a5688e9d9ed8c8dd2166de6d00efd2c
Joselu <joseluissgz97@gmail.com>
2024-05-28 23:12:12 +0200

    refactor: Update BookingDetails component to fetch and display room details

--- src/pages/BookingDetails.tsx
+++ src/pages/BookingDetails.tsx
@@ -1,21 +1,41 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { AppDispatch, RootState } from '../app/store';
 import { getBooking } from '../features/bookinsSlice/bookinsThunk';
+import { getRomById } from '../features/roomsSlice/roomsThunk';
 
 const BookingDetails = () => {
-  const { bookin, loading } = useSelector((state: RootState) => state.bookings);
+  const [isLoading, setIsLoading] = useState(true);
+  const { bookin } = useSelector((state: RootState) => state.bookings);
+  const { room } = useSelector((state: RootState) => state.rooms);
+
   const { id } = useParams<{ id: string }>();
   const distpatch = useDispatch<AppDispatch>();
 
   useEffect(() => {
-    distpatch(getBooking(id!));
-  }, [id, distpatch]);
+    const getBookingByID = async () => {
+      await distpatch(getBooking(id!));
+      setIsLoading(false);
+    };
+    getBookingByID();
+  }, []);
+
+  useEffect(() => {
+    const getRoomByID = async () => {
+      if (bookin?.roomID) {
+        console.log('GOLAAA');
+        await distpatch(getRomById(bookin?.roomID));
+      }
+    };
+    getRoomByID();
+  }, [bookin?.roomID, distpatch]);
+  if (isLoading) return <p>Loading...</p>;
 
   return (
     <section>
-      {loading === 'pending' ? <p>Loading...</p> : <p>{bookin?.guest && bookin.guest?.name}</p>}
+      <span>{bookin?.guest.name}</span>
+      <span>{room?.amenities}</span>
     </section>
   );
 };
