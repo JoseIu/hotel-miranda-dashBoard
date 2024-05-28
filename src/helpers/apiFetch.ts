987a98efef80867c0f1fd8a4d129f25b28e859c4
Joselu <joseluissgz97@gmail.com>
2024-05-28 23:10:07 +0200

    refactor: Update apiFetch to include optional id parameter in URL path

--- src/helpers/apiFetch.ts
+++ src/helpers/apiFetch.ts
@@ -1,8 +1,8 @@
-const apiFetch = async (path: string, method: string = 'GET') => {
+const apiFetch = async (path: string, method: string = 'GET', id?: string) => {
   const BASE_URL = import.meta.env.VITE_BACK_URL;
 
   const token = localStorage.getItem('token');
-  const response = await fetch(`${BASE_URL}${path}`, {
+  const response = await fetch(`${BASE_URL}/${path}/${id ? id : ''}`, {
     method,
     headers: {
       'Content-Type': 'application/json',
