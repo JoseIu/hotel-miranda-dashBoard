7eba38ac0fc79665959f0ece7f77dff594e71bbd
Joselu <joseluissgz97@gmail.com>
2024-05-29 23:23:00 +0200

    refactor: add Booking details info

--- src/pages/BookingDetails.tsx
+++ src/pages/BookingDetails.tsx
@@ -1,7 +1,10 @@
 import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import styled from 'styled-components';
 import { AppDispatch, RootState } from '../app/store';
+import Header from '../components/Header';
+import { ContainerSection } from '../components/shared/StyledComponets';
 import { getBooking } from '../features/bookinsSlice/bookinsThunk';
 import { getRomById } from '../features/roomsSlice/roomsThunk';
 
@@ -19,7 +22,7 @@ const BookingDetails = () => {
       setIsLoading(false);
     };
     getBookingByID();
-  }, []);
+  }, [id, distpatch]);
 
   useEffect(() => {
     const getRoomByID = async () => {
@@ -33,11 +36,173 @@ const BookingDetails = () => {
   if (isLoading) return <p>Loading...</p>;
 
   return (
-    <section>
-      <span>{bookin?.guest.name}</span>
-      <span>{room?.amenities}</span>
-    </section>
+    <ContainerSection>
+      <Header title={'Bookings Details'} />
+      <BookingContainer>
+        <BookingDetail>
+          <BookingInfo>
+            <UserInfo>
+              <UseData>
+                <img src={bookin?.guest.img} alt={bookin?.guest.name} />
+
+                <h2>
+                  {bookin?.guest.name} <span>ID: {bookin?.guest.reservationID}</span>
+                </h2>
+              </UseData>
+              <Check>
+                <BookingRow>
+                  Check In
+                  <span>
+                    {bookin?.checkin.date.slice(0, 10)} | {bookin?.checkin.time}
+                  </span>
+                </BookingRow>
+                <BookingRow>
+                  Check Out
+                  <span>
+                    {bookin?.checkOut.date.slice(0, 10)} | {bookin?.checkOut.time}
+                  </span>
+                </BookingRow>
+              </Check>
+            </UserInfo>
+            <RoomInfo>
+              <RoomData>
+                <BookingRow>
+                  <span>Room Info</span>
+                  <h2>
+                    {room?.roomType} - {room?.roomNumber}
+                  </h2>
+                </BookingRow>
+                <BookingRow>
+                  <span>Price</span>
+                  <h2>
+                    $ {room?.price} <span>/night</span>
+                  </h2>
+                </BookingRow>
+              </RoomData>
+
+              <p>{room?.description}</p>
+
+              <BookingRow>
+                <span>Facilities</span>
+                <AmenitiesList>
+                  {room?.amenities.map((amenity) => (
+                    <li key={amenity}>{amenity}</li>
+                  ))}
+                </AmenitiesList>
+              </BookingRow>
+            </RoomInfo>
+          </BookingInfo>
+
+          <RoomImages>
+            <img src={room?.roomImages} alt="" />
+          </RoomImages>
+        </BookingDetail>
+      </BookingContainer>
+    </ContainerSection>
   );
 };
 
 export default BookingDetails;
+const BookingContainer = styled.div`
+  padding: 3em 4em 3em 4em;
+`;
+const BookingDetail = styled.article`
+  padding: 2rem;
+  border-radius: 0.5rem;
+  background-color: #18181b;
+  /* background-color: linear-gradient(145deg, #161618, #1a1a1d); */
+
+  box-shadow: 5px 5px 10px #141417, -5px -5px 10px #1c1c1f;
+  display: grid;
+  gap: 4rem;
+  grid-template-columns: repeat(2, 1fr);
+`;
+const BookingInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  row-gap: 2rem;
+`;
+
+const UserInfo = styled.div`
+  padding: 0 0 1.5rem 0;
+  display: flex;
+  flex-direction: column;
+  row-gap: 1.5rem;
+  border-bottom: 1px solid #ccc;
+`;
+const UseData = styled.div`
+  display: flex;
+  gap: 3rem;
+  img {
+    border-radius: 0.5rem;
+    max-width: 10rem;
+  }
+  h2 {
+    display: flex;
+    flex-direction: column;
+    row-gap: 0.5rem;
+    span {
+      color: green;
+      font-size: 0.8em;
+    }
+  }
+`;
+
+const Check = styled.div`
+  display: flex;
+  justify-content: space-between;
+  span {
+    display: flex;
+    flex-direction: column;
+    row-gap: 0.5em;
+    span {
+      font-weight: 600;
+    }
+  }
+`;
+
+const RoomInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  row-gap: 1.5rem;
+`;
+
+const RoomData = styled.div`
+  display: flex;
+  justify-content: space-between;
+
+  span {
+    font-size: 0.9em;
+  }
+  h2 {
+    font-size: 1.3em;
+    font-weight: 600;
+    span {
+      color: green;
+      font-size: 0.7em;
+    }
+  }
+`;
+const BookingRow = styled.div`
+  display: flex;
+  flex-direction: column;
+  row-gap: 1rem;
+`;
+const AmenitiesList = styled.ul`
+  display: flex;
+  gap: 1rem;
+  li {
+    padding: 0.2rem 0.5rem;
+    border-radius: 0.5rem;
+    font-size: 0.9em;
+    background-color: white;
+    color: black;
+  }
+`;
+
+const RoomImages = styled.div`
+  align-self: flex-end;
+  img {
+    max-width: 30rem;
+  }
+`;
