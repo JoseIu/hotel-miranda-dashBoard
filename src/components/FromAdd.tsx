c9e22a780dec879eed7a14398d140a6d6872ef3a
Joselu <joseluissgz97@gmail.com>
2024-05-30 22:42:10 +0200

    refactor: Update roomsSlice to use RoomInterface instead of Room

--- src/components/FromAdd.tsx
+++ src/components/FromAdd.tsx
@@ -1,149 +1,151 @@
-import { Dispatch, useState } from 'react';
+import { useDispatch } from 'react-redux';
 import styled from 'styled-components';
+import { AppDispatch } from '../app/store';
+import { addBooking } from '../features/bookinsSlice/bookinsThunk';
 import { getRandomId } from '../helpers/getRandomId';
-interface FromAddProps {
-  modalIsOpen: boolean;
-  setModalIsOpen: Dispatch<React.SetStateAction<boolean>>;
-}
+import { BookingToSendInterface, Status } from '../interfaces/booking.interface';
+import { FormInterface } from '../pages/BookingDeleteAdd';
 
-const FromAdd = ({ modalIsOpen, setModalIsOpen }: FromAddProps) => {
-  const {
-    form,
-    setName,
-    setLastName,
-    setOrderDate,
-    setCheckIn,
-    setCheckOut,
-    setRoomType,
-    setStatus,
-    setSpecialRequest,
-  } = useForm();
-  console.log(form);
+const FromAdd = ({
+  form,
+  setName,
+  setLastName,
+  setOrderDate,
+  setCheckIn,
+  setCheckOut,
+  setSpecialRequest,
+  setRoomType,
+  setStatus,
+}: FormInterface) => {
+  const dispatch = useDispatch<AppDispatch>();
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
-    if (Object.values(form).some((value) => value === '')) return alert('Please fill all the fields');
-    const checkInFormat = form.checkIn;
-    const [dateIn, timeIn] = checkInFormat.split('T');
-    const checkOutFormat = form.checkIn;
-    const [dateOut, timeOut] = checkOutFormat.split('T');
-    const newBooking = {
-      orderDate: form.orderDate,
+    // if (Object.values(form).some((value) => value === '')) return alert('Please fill all the fields');
+
+    const newBooking: BookingToSendInterface = {
+      guest: {
+        name: form.guest.name,
+        lastName: form.guest.lastName,
+        reservationID: getRandomId(),
+        img: form.guest.img,
+      },
       checkin: {
-        date: dateIn,
-        time: timeIn,
+        date: form.checkin.date,
+        time: form.checkin.time,
       },
       checkOut: {
-        date: dateOut,
-        time: timeOut,
+        date: form.checkOut.date,
+        time: form.checkOut.date,
       },
+      orderDate: form.orderDate,
+      roomNumber: getRandomId(),
+      roomID: '665606936db0eccd9afd932a',
       specialRequest: form.specialRequest,
       roomType: form.roomType,
-      status: form.status,
-      guest: {
-        name: form.name,
-        lastName: form.lastName,
-        reservationID: getRandomId(),
-        img: form.image,
-      },
+      status: form.status as Status,
     };
 
-    console.log(newBooking);
-    setModalIsOpen(!modalIsOpen);
+    await dispatch(addBooking(newBooking));
   };
+
   return (
-    <FormContainert $modalIsOpen={modalIsOpen}>
-      <Form onSubmit={handleSubmit}>
-        <GuestData>
-          <label htmlFor="name">
-            Name
-            <input
-              type="text"
-              name="name"
-              id="name"
-              value={form.name}
-              onChange={(event) => setName(event.target.value)}
-            />
-          </label>
+    <Form onSubmit={handleSubmit}>
+      <Fieldset $colums={3}>
+        <legend>GUEST DATA</legend>
+        <label htmlFor="name">
+          Name
+          <input
+            type="text"
+            name="name"
+            id="name"
+            value={form.guest.name}
+            onChange={(event) => setName(event.target.value)}
+          />
+        </label>
 
-          <label htmlFor="lastName">
-            last Name
-            <input
-              type="text"
-              name="lastName"
-              id="lastName"
-              value={form.lastName}
-              onChange={(event) => setLastName(event.target.value)}
-            />
-          </label>
-        </GuestData>
+        <label htmlFor="lastName">
+          last Name
+          <input
+            type="text"
+            name="lastName"
+            id="lastName"
+            value={form.guest.lastName}
+            onChange={(event) => setLastName(event.target.value)}
+          />
+        </label>
+      </Fieldset>
 
-        <CheckDATA>
-          <label htmlFor="orderDate">
-            Order Date
-            <input
-              type="date"
-              name="orderDate"
-              id="orderDate"
-              value={form.orderDate}
-              onChange={(event) => setOrderDate(event.target.value)}
-            />
-          </label>
+      <Fieldset $colums={3}>
+        <legend>Date</legend>
+        <label htmlFor="orderDate">
+          Order Date
+          <input
+            type="date"
+            name="orderDate"
+            id="orderDate"
+            value={form.orderDate}
+            onChange={(event) => setOrderDate(event.target.value)}
+          />
+        </label>
 
-          <label htmlFor="checkinDate">
-            Checkin Date
-            <input
-              type="datetime-local"
-              name="checkinDate"
-              id="checkinDate"
-              value={form.checkIn}
-              onChange={(event) => setCheckIn(event.target.value)}
-            />
-          </label>
-          <label htmlFor="checkOut">
-            Check Out
-            <input
-              type="datetime-local"
-              name="checkOut"
-              id="checkOut"
-              value={form.checkOut}
-              onChange={(event) => setCheckOut(event.target.value)}
-            />
-          </label>
-        </CheckDATA>
+        <label htmlFor="checkinDate">
+          Checkin Date
+          <input
+            type="datetime-local"
+            name="checkinDate"
+            id="checkinDate"
+            value={`${form.checkin.date}T${form.checkin.time}`}
+            onChange={(event) => setCheckIn(event.target.value)}
+          />
+        </label>
+        <label htmlFor="checkOut">
+          Check Out
+          <input
+            type="datetime-local"
+            name="checkOut"
+            id="checkOut"
+            value={`${form.checkOut.date}T${form.checkOut.time}`}
+            onChange={(event) => setCheckOut(event.target.value)}
+          />
+        </label>
+      </Fieldset>
+      <Fieldset $colums={2}>
+        <legend>Room</legend>
 
-        <Select>
-          <label htmlFor="bedType">
-            Bed Type
-            <select
-              name="bedType"
-              id="bedType"
-              value={form.roomType}
-              onChange={(event) => setRoomType(event.target.value)}
-            >
-              <option value="Single Bed">Single Bed</option>
-              <option value="Double Bed">Double Bed</option>
-              <option value="Double Superior">Double Superior</option>
-              <option value="Suite">Suite</option>
-            </select>
-          </label>
+        <label htmlFor="bedType">
+          Room Type
+          <select
+            name="roomType"
+            id="bedType"
+            value={form.roomType}
+            onChange={(event) => setRoomType(event.target.value)}
+          >
+            <option value="Single Bed">Single Bed</option>
+            <option value="Double Bed">Double Bed</option>
+            <option value="Double Superior">Double Superior</option>
+            <option value="Suite">Suite</option>
+          </select>
+        </label>
 
-          <label htmlFor="status">
-            Status
-            <select
-              name="status"
-              id="status"
-              value={form.status}
-              onChange={(event) => setStatus(event.target.value)}
-            >
-              <option value="Check In">Check In</option>
-              <option value="Check Out">Check Out</option>
-              <option value="In Progress">In Progress</option>
-            </select>
-          </label>
-        </Select>
+        <label htmlFor="status">
+          Status
+          <select
+            name="status"
+            id="status"
+            value={form.status}
+            onChange={(event) => setStatus(event.target.value as Status)}
+          >
+            <option value="Check In">Check In</option>
+            <option value="Check Out">Check Out</option>
+            <option value="In Progress">In Progress</option>
+          </select>
+        </label>
+      </Fieldset>
 
+      <label htmlFor="specialRequest">
+        SpecialRequest
         <textarea
           name="specialRequest"
           id="specialRequest"
@@ -152,77 +154,18 @@ const FromAdd = ({ modalIsOpen, setModalIsOpen }: FromAddProps) => {
           value={form.specialRequest}
           onChange={(event) => setSpecialRequest(event.target.value)}
         ></textarea>
+      </label>
 
-        <button type="submit">Add new Bookin</button>
-
-        <ButtonCloseModal type="button" onClick={() => setModalIsOpen(!modalIsOpen)}>
-          X
-        </ButtonCloseModal>
-      </Form>
-    </FormContainert>
+      <button type="submit">Add new Bookin</button>
+    </Form>
   );
 };
 
 export default FromAdd;
 
-const useForm = () => {
-  const [form, setForm] = useState({
-    name: '',
-    lastName: '',
-    orderDate: '',
-    image: '/images/user2.webp',
-    checkIn: '',
-    checkOut: '',
-    roomType: '',
-    specialRequest: '',
-    status: '',
-  });
-
-  const setName = (name: string) => setForm({ ...form, name });
-
-  const setLastName = (lastName: string) => setForm({ ...form, lastName });
-
-  const setOrderDate = (orderDate: string) => setForm({ ...form, orderDate });
-  const setCheckIn = (checkIn: string) => setForm({ ...form, checkIn });
-
-  const setCheckOut = (checkOut: string) => setForm({ ...form, checkOut });
-
-  const setRoomType = (roomType: string) => setForm({ ...form, roomType });
-
-  const setSpecialRequest = (specialRequest: string) => setForm({ ...form, specialRequest });
-
-  const setStatus = (status: string) => setForm({ ...form, status });
-
-  return {
-    form,
-    setName,
-    setLastName,
-    setOrderDate,
-    setCheckIn,
-    setCheckOut,
-    setRoomType,
-    setSpecialRequest,
-    setStatus,
-  };
-};
-const FormContainert = styled.div<{ $modalIsOpen: boolean }>`
-  position: absolute;
-  top: 0;
-  left: 0;
-  height: 100%;
-  width: 100%;
-  background-color: #050505e1;
-  transition: all 0.3s ease;
-  transform: scale(${(props) => (props.$modalIsOpen ? '1' : '0')});
-
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
-
 const Form = styled.form`
   position: relative;
-  max-width: 50rem;
+  max-width: 80rem;
   padding: 2rem;
   border: 0.0625rem solid #135846;
   border-radius: 0.4rem;
@@ -254,41 +197,11 @@ const Form = styled.form`
   }
 `;
 
-const GuestData = styled.div`
+const Fieldset = styled.fieldset<{ $colums: number }>`
+  border: 0.0625rem solid #135846;
+  border-radius: 0.4rem;
+  padding: 1rem;
   display: grid;
-  grid-template-columns: repeat(2, 1fr);
+  grid-template-columns: repeat(${(props) => props.$colums}, 1fr);
   gap: 2rem;
-`;
-
-const CheckDATA = styled.div`
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  gap: 2rem;
-`;
-
-const Select = styled.div`
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  gap: 2rem;
-
-  option {
-    background-color: #222;
-  }
-`;
-const ButtonCloseModal = styled.button`
-  position: absolute;
-  top: -1rem;
-  right: -0.5rem;
-  width: 2rem;
-  height: 2rem;
-  border-radius: 50% !important;
-  transition: all 0.3s ease;
-
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  cursor: pointer;
-  &:hover {
-    transform: scale(1.1);
-  }
 `;
