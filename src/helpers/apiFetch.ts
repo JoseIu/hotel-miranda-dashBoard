3d477e1ece6f189839cfb4eaa70c540d1c4616d4
Joselu <joseluissgz97@gmail.com>
2024-05-30 22:42:47 +0200

    refactor: Add support for sending data in apiFetch function

--- src/helpers/apiFetch.ts
+++ src/helpers/apiFetch.ts
@@ -1,14 +1,19 @@
-const apiFetch = async (path: string, method: string = 'GET', id?: string) => {
+const apiFetch = async <T>(path: string, method: string = 'GET', id?: string | null, data?: T) => {
   const BASE_URL = import.meta.env.VITE_BACK_URL;
 
   const token = localStorage.getItem('token');
-  const response = await fetch(`${BASE_URL}/${path}/${id ? id : ''}`, {
+
+  const fetchOptions: RequestInit = {
     method,
     headers: {
       'Content-Type': 'application/json',
       Authorization: `Bearer ${token}`,
     },
-  });
+  };
+  if (data) {
+    fetchOptions.body = JSON.stringify(data);
+  }
+  const response = await fetch(`${BASE_URL}/${path}/${id ? id : ''}`, fetchOptions);
 
   return response.json();
 };
