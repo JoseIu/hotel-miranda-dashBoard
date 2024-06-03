7ae39a9f02cedeb60f14b7709606611a7cc05d3c
Joselu <joseluissgz97@gmail.com>
2024-06-03 22:51:21 +0200

    refactor: Update BookingDeleteAdd component to use BookingForm component

--- src/pages/BookingDeleteAdd.tsx
+++ src/pages/BookingDeleteAdd.tsx
@@ -1,179 +1,40 @@
 import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
 import styled from 'styled-components';
 import { RootState } from '../app/store';
-import FormBookingEdit from '../components/FormBookingEdit';
-import FromAdd from '../components/FromAdd';
 import Header from '../components/Header';
+import BookingForm from '../components/bookingForm/BookingForm';
 import { ContainerSection } from '../components/shared/StyledComponets';
-import { BookingInterface, Status } from '../interfaces/booking.interface';
+import { BookingInterface } from '../interfaces/booking.interface';
 
 const BookingDeleteAdd = () => {
   const { id } = useParams<{ id: string }>();
   const [isEditing, setIsEditing] = useState(false);
+  const [bookingToEdit, setBookingToEdit] = useState<BookingInterface | null>(null);
   const { bookins } = useSelector((state: RootState) => state.bookings);
 
-  const {
-    form,
-    setName,
-    setLastName,
-    setOrderDate,
-    setCheckIn,
-    setCheckOut,
-    setRoomType,
-    setStatus,
-    setSpecialRequest,
-    setFormValues,
-  } = useForm();
   useEffect(() => {
     if (id) {
       setIsEditing(true);
       const booking = bookins.find((booking) => booking._id === id);
-      console.log(booking);
       if (!booking) return;
-      setFormValues(booking);
+      setBookingToEdit(booking);
     }
-  }, []);
+  }, [id, bookins]);
 
   return (
     <ContainerSection>
       <Header title={isEditing ? 'Editing' : 'Add Booking'} />
-      <FormsContainer>
-        {isEditing ? (
-          <FormBookingEdit
-            form={form}
-            setName={setName}
-            setLastName={setLastName}
-            setOrderDate={setOrderDate}
-            setCheckIn={setCheckIn}
-            setCheckOut={setCheckOut}
-            setRoomType={setRoomType}
-            setStatus={setStatus}
-            setSpecialRequest={setSpecialRequest}
-          />
-        ) : (
-          <FromAdd
-            form={form}
-            setName={setName}
-            setLastName={setLastName}
-            setOrderDate={setOrderDate}
-            setCheckIn={setCheckIn}
-            setCheckOut={setCheckOut}
-            setRoomType={setRoomType}
-            setStatus={setStatus}
-            setSpecialRequest={setSpecialRequest}
-          ></FromAdd>
-        )}
-        <ToastContainer
-          position="top-right"
-          autoClose={1500}
-          hideProgressBar={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-          theme="dark"
-        />
-      </FormsContainer>
+      <FormsContainer>{isEditing ? <BookingForm booking={bookingToEdit} /> : <BookingForm />}</FormsContainer>
     </ContainerSection>
   );
 };
 
 export default BookingDeleteAdd;
 
-export interface FormInterface {
-  form: BookingInterface;
-  setName: (name: string) => void;
-  setLastName: (lastName: string) => void;
-  setIamge?: (image: string) => void;
-  setOrderDate: (orderDate: string) => void;
-  setCheckIn: (checkIn: string) => void;
-  setCheckOut: (checkOut: string) => void;
-  setRoomType: (roomType: string) => void;
-  setSpecialRequest: (specialRequest: string) => void;
-  setStatus: (status: Status) => void;
-}
 const FormsContainer = styled.div`
   padding-top: 3rem;
   display: flex;
   justify-content: center;
 `;
-
-const useForm = () => {
-  const [form, setForm] = useState<BookingInterface>({
-    _id: '',
-    guest: {
-      name: '',
-      lastName: '',
-      reservationID: '',
-      img: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/664.jpg',
-    },
-    checkin: {
-      date: '',
-      time: '',
-    },
-    checkOut: {
-      date: '',
-      time: '',
-    },
-    orderDate: '',
-    roomType: '',
-    roomNumber: 'Single Bed',
-    roomID: '',
-    specialRequest: '',
-    status: 'Check In',
-  });
-
-  const setName = (name: string) => setForm({ ...form, guest: { ...form.guest, name } });
-
-  const setLastName = (lastName: string) => setForm({ ...form, guest: { ...form.guest, lastName } });
-  const setIamge = (img: string) => setForm({ ...form, guest: { ...form.guest, img } });
-
-  const setOrderDate = (orderDate: string) => setForm({ ...form, orderDate });
-
-  const setCheckIn = (checkIn: string) => {
-    const checkInFormat = checkIn;
-    const [dateIn, timeIn] = checkInFormat.split('T');
-    setForm({ ...form, checkin: { date: dateIn, time: timeIn } });
-  };
-
-  const setCheckOut = (checkOut: string) => {
-    const checkInFormat = checkOut;
-    const [dateIn, timeIn] = checkInFormat.split('T');
-    setForm({ ...form, checkOut: { date: dateIn, time: timeIn } });
-  };
-
-  const setRoomType = (roomType: string) => setForm({ ...form, roomType });
-
-  const setSpecialRequest = (specialRequest: string) => setForm({ ...form, specialRequest });
-
-  const setStatus = (status: Status) => setForm({ ...form, status });
-
-  const setFormValues = (newForm: BookingInterface) => {
-    const { checkin, checkOut } = newForm;
-    const checkInFormat = checkin.date.slice(0, 10);
-    const checkOutFormat = checkOut.date.slice(0, 10);
-    setForm({
-      ...newForm,
-      checkin: { date: checkInFormat, time: checkin.time },
-      checkOut: { date: checkOutFormat, time: checkOut.time },
-    });
-  };
-
-  return {
-    form,
-    setName,
-    setLastName,
-    setIamge,
-    setOrderDate,
-    setCheckIn,
-    setCheckOut,
-    setRoomType,
-    setSpecialRequest,
-    setStatus,
-    setFormValues,
-  };
-};
