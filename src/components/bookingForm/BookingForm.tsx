eba3b5e012fbd2122212cafa2312c337ba32ddd2
Joselu <joseluissgz97@gmail.com>
2024-06-04 22:41:35 +0200

    refactor: adding handleSubmit and HandleEdit and refactoring structure form

--- src/components/bookingForm/BookingForm.tsx
+++ src/components/bookingForm/BookingForm.tsx
@@ -1,19 +1,25 @@
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useEffect } from 'react';
 import { SubmitHandler, useForm } from 'react-hook-form';
+import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
-import { BookingInterface } from '../../interfaces/booking.interface';
+import { AppDispatch } from '../../app/store';
+import { addBooking, updateBooking } from '../../features/bookinsSlice/bookinsThunk';
+import { getRandomId } from '../../helpers/getRandomId';
+import { BookingInterface, BookingToSendInterface, Status } from '../../interfaces/booking.interface';
 import { RoomType } from '../../interfaces/room.interface';
 import { Form, FormSubmmit, SelectOption } from '../shared/GlobalStyle';
+import { FormRow } from '../shared/StyledComponets';
 import Input from '../shared/input/Input';
 import Select from '../shared/select/Select';
 import { BookingSchema, bookingShema } from './bookingSchema';
+
 type BookingFormProps = {
   booking?: BookingInterface | null;
 };
 const BookingForm = ({ booking }: BookingFormProps) => {
-  //   const dispatch = useDispatch<AppDispatch>();
+  const dispatch = useDispatch<AppDispatch>();
   const navigate = useNavigate();
   const {
     register,
@@ -26,18 +32,67 @@ const BookingForm = ({ booking }: BookingFormProps) => {
 
   const onHandleSubmit: SubmitHandler<BookingSchema> = async (data) => {
     console.log('ADDED');
-    console.log(data);
+
+    const bookingToSend: BookingToSendInterface = {
+      guest: {
+        name: data.name,
+        lastName: data.lastName,
+        reservationID: getRandomId(),
+        img: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/183.jpg',
+      },
+      orderDate: data.orderDate,
+      checkin: {
+        date: data.checkin.split('T')[0],
+        time: data.checkin.split('T')[1],
+      },
+      checkOut: {
+        date: data.checkOut.split('T')[0],
+        time: data.checkOut.split('T')[1],
+      },
+      roomType: data.roomType as RoomType,
+      roomNumber: getRandomId(),
+      roomID: '6655ff57f926766f49be6ce4',
+      specialRequest: data.specialRequest,
+      status: data.status as Status,
+    };
+    console.log(bookingToSend);
+    await dispatch(addBooking(bookingToSend));
     toast.success('Added Successfully!');
     navigate('/admin/bookings');
   };
   const onHandleEdit: SubmitHandler<BookingSchema> = async (data) => {
     console.log(data);
     console.log('EDITED');
+    const bookingToEdit: BookingInterface = {
+      _id: booking!._id,
+      guest: {
+        name: data.name,
+        lastName: data.lastName,
+        reservationID: booking!.guest.reservationID,
+        img: booking!.guest.img,
+      },
+      orderDate: data.orderDate,
+      checkin: {
+        date: data.checkin.split('T')[0],
+        time: data.checkin.split('T')[1],
+      },
+      checkOut: {
+        date: data.checkOut.split('T')[0],
+        time: data.checkOut.split('T')[1],
+      },
+      roomType: data.roomType as RoomType,
+      roomNumber: booking!.roomNumber,
+      roomID: booking!.roomID,
+      specialRequest: data.specialRequest,
+      status: data.status as Status,
+    };
+    await dispatch(updateBooking(bookingToEdit));
+    toast.success('Added Successfully!');
+    navigate('/admin/bookings');
   };
 
   useEffect(() => {
     if (!booking) return;
-    console.log(booking);
     setValue('name', booking.guest.name);
     setValue('lastName', booking.guest.lastName);
     setValue('orderDate', booking.orderDate.split('T')[0]);
@@ -45,57 +100,76 @@ const BookingForm = ({ booking }: BookingFormProps) => {
     setValue('checkOut', `${booking.checkOut.date.split('T')[0]}T${booking.checkOut.time}`);
     setValue('roomType', booking.roomType as RoomType);
     setValue('status', booking.status);
+    setValue('specialRequest', booking.specialRequest);
   }, [booking, setValue]);
   return (
-    <Form onSubmit={handleSubmit(booking !== null ? onHandleEdit : onHandleSubmit)}>
-      <Input
-        label="Description"
-        error={errors['name']}
-        id="name"
-        type="text"
-        placeholder="Write name"
-        {...register('name')}
-      />
+    <Form onSubmit={handleSubmit(booking !== undefined ? onHandleEdit : onHandleSubmit)}>
+      <FormRow>
+        <Input
+          label="Name"
+          error={errors['name']}
+          id="name"
+          type="text"
+          placeholder="Write name"
+          {...register('name')}
+        />
 
+        <Input
+          label="Last Name"
+          type="text"
+          id="lastName"
+          error={errors['lastName']}
+          placeholder="lastName"
+          {...register('lastName')}
+        />
+      </FormRow>
+      <FormRow>
+        <Input
+          label="Order Date"
+          type="date"
+          id="price"
+          error={errors['orderDate']}
+          placeholder="orderDate"
+          {...register('orderDate')}
+        />
+        <Input
+          label="CheckIn"
+          type="datetime-local"
+          id="checkin"
+          error={errors['checkin']}
+          placeholder="checkin"
+          {...register('checkin')}
+        />
+        <Input
+          label="check Out"
+          type="datetime-local"
+          id="checkOut"
+          error={errors['checkOut']}
+          placeholder="checkOut"
+          {...register('checkOut')}
+        />
+      </FormRow>
+      <FormRow>
+        <Select label="Room Type" id="roomType" error={errors['roomType']} {...register('roomType')}>
+          <SelectOption value="Single Bed">Single Bed</SelectOption>
+          <SelectOption value="Double Bed">Double Bed</SelectOption>
+          <SelectOption value="Double Superior">Double Superior</SelectOption>
+          <SelectOption value="Suite">Suite</SelectOption>
+        </Select>
+        <Select label="Status" id="roomType" error={errors['status']} {...register('status')}>
+          <SelectOption value="Check In">Check In</SelectOption>
+          <SelectOption value="Check Out">Check Out</SelectOption>
+          <SelectOption value="In Progress">In Progressr</SelectOption>
+        </Select>
+      </FormRow>
       <Input
-        label="Last Name"
+        label="Special Request"
         type="text"
-        id="lastName"
-        error={errors['lastName']}
-        placeholder="lastName"
-        {...register('lastName')}
+        id="specialRequest"
+        error={errors['specialRequest']}
+        placeholder="specialRequest"
+        {...register('specialRequest')}
       />
-      <Input
-        label="Order Date"
-        type="date"
-        id="price"
-        error={errors['orderDate']}
-        placeholder="orderDate"
-        {...register('orderDate')}
-      />
-      <Input
-        label="CheckIn"
-        type="datetime-local"
-        id="checkin"
-        error={errors['checkin']}
-        placeholder="checkin"
-        {...register('checkin')}
-      />
-      <Input
-        label="check Out"
-        type="datetime-local"
-        id="checkOut"
-        error={errors['checkOut']}
-        placeholder="checkOut"
-        {...register('checkOut')}
-      />
-      <Select label="Room Type" id="roomType" error={errors['roomType']} {...register('roomType')}>
-        <SelectOption value="Single Bed">Single Bed</SelectOption>
-        <SelectOption value="Double Bed">Double Bed</SelectOption>
-        <SelectOption value="Double Superior">Double Superior</SelectOption>
-        <SelectOption value="Suite">Suite</SelectOption>
-      </Select>
-
       <FormSubmmit type="submit">{booking ? 'Edit' : 'Add'}</FormSubmmit>
     </Form>
   );
