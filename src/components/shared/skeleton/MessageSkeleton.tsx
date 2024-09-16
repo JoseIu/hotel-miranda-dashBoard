0817dfb93b70f1d88556fca80acc82078e0ebfaf
Joselu <joseluissgz97@gmail.com>
2024-09-16 22:05:19 +0200

    feat: add MessageSkeleton

--- src/components/shared/skeleton/MessageSkeleton.tsx
+++ src/components/shared/skeleton/MessageSkeleton.tsx
@@ -1,3 +1,39 @@
+import styled from 'styled-components';
+
 export const MessageSkeleton = () => {
-  return <div>MessageSkeleton</div>;
+  return (
+    <MessageContainerSkeleton>
+      <MessageCardSkeleton></MessageCardSkeleton>
+      <MessageCardSkeleton></MessageCardSkeleton>
+      <MessageCardSkeleton></MessageCardSkeleton>
+      <MessageCardSkeleton></MessageCardSkeleton>
+    </MessageContainerSkeleton>
+  );
 };
+const MessageContainerSkeleton = styled.section`
+  width: 100%;
+  margin-left: auto;
+  margin-right: auto;
+
+  display: flex;
+  gap: 1.5rem;
+`;
+const MessageCardSkeleton = styled.div`
+  width: calc(100% / 4);
+  border-radius: 0.3rem;
+  aspect-ratio: 16/9;
+
+  display: flex;
+  flex-direction: column;
+  row-gap: 0.5rem;
+
+  animation: skeleton-loading 1s infinite alternate;
+  @keyframes skeleton-loading {
+    0% {
+      background-color: var(--zinc-900);
+    }
+    100% {
+      background-color: var(--zinc-800);
+    }
+  }
+`;
